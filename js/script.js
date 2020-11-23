825930261b1d72f3704970dde8a64ec53dcbf3e9
Roberto <rdrago.uni@gmail.com>
2020-11-23 12:18:48 +0100

    data structure wip

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var app = new Vue ({
+
+    el: '#app',
+    data: {
+        id_users: [
+            {name: 'Francesco', lastlogin: 'Ultimo accesso oggi 21/11/2020, alle 17:55'},
+            {name: 'Marco', lastlogin: 'Ultimo accesso oggi 20/11/2020, alle 9:20'},
+            {name: 'Rita', lastlogin: 'Ultimo accesso oggi 22/11/2020, alle 7:30'},
+            {name: 'Claudio', lastlogin: 'Ultimo accesso oggi 24/11/2020, alle 12:00'}
+        ]
+    }
+})
